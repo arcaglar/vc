04f344f274c94a486adabfbe12b4f1f33dc651c9
arcaglar <arcaglar@gmail.com>
2022-09-15 02:13:44 +0300

    add tab item conditional class

--- components/Tab.vue
+++ components/Tab.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="container">
     <ul class="tab-bar">
-      <li class="tab-bar__item" v-for="item in data"> {{ item }} </li>
+      <li :class="[{'tab-bar__item--active': getCategory === item}, 'tab-bar__item']" v-for="item in data" @click="setCategory(item)"> {{ item }} </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'Tab',
   props: {
     data: Array
+  },
+  computed: {
+    ...mapGetters([
+      'getCategory'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'setCategory'
+    ])
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,10 +1,23 @@
 export const state = () => ({
+    selectedCategory: 'jewelery'
 })
 
 export const getters = {
+    getCategory(state) {
+        return state.selectedCategory
+    }
+}
+
+export const actions = {
+    setCategory (context, payload) {
+        console.log(payload)
+        context.commit('setCategory', payload)
+    }
 }
 
 export const mutations = {
+    setCategory (state, payload) {
+        state.selectedCategory = payload
+    }
 }
 
-export const actions = {}
