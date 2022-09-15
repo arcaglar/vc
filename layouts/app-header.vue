421c809eda92261904f31c56d5cd39b32e008aef
arcaglar <arcaglar@gmail.com>
2022-09-15 18:04:59 +0300

    minor change

--- layouts/app-header.vue
+++ layouts/app-header.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="header">
         <div class="container">
-            <img class="header__logo" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/ziydsqwklaqebu7axgtu" alt="logo">
+            <NuxtLink :to="{ path: '/' }">
+                <img class="header__logo" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/ziydsqwklaqebu7axgtu" alt="logo">
+            </NuxtLink>
         </div>
     </div>
 </template>
